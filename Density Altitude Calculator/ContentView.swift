8cefeb80ff5c9a21c8f95276c426dceb4990664f
Kurt Silsby <kdsilsby@gmail.com>
2024-08-21 15:30:56 -0400

    Version 0.1 - Updated basic input fields required for dry density altitude calculation

--- Density Altitude Calculator/ContentView.swift
+++ Density Altitude Calculator/ContentView.swift
@@ -8,12 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var elevation = 0
+    @State private var temperature = 15
+    @State private var altimeter = 29.92
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            HStack {
+                Text("Field Elevation: ")
+                TextField("Field Elevation", value: $elevation, format: .number)
+            }
+            HStack {
+                Text("Temperature: ")
+                TextField("Temperature in C", value: $temperature, format: .number)
+            }
+            HStack {
+                Text("Altimeter: ")
+                TextField("Altimeter in inHG", value: $altimeter, format: .number)
+            }
+            HStack {
+                Text("Dry Density Altitude Calculator")
+            }
+            
         }
         .padding()
     }
