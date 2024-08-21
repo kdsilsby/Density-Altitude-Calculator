5a74fd8e36a55c6cc8c6a55b45ef44e6f861782b
Kurt Silsby <kdsilsby@hotmail.com>
2024-08-21 14:23:06 -0400

    Initial Commit

---
+++ Density Altitude Calculator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Density Altitude Calculator
+//
+//  Created by Kurt Silsby on 8/21/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Density Altitude Calculator/Density_Altitude_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Density_Altitude_CalculatorApp.swift
+//  Density Altitude Calculator
+//
+//  Created by Kurt Silsby on 8/21/24.
+//
+
+import SwiftUI
+
+@main
+struct Density_Altitude_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
