dc0ca8bbd032d266a8b2a733c2417f36f37c5d81
Kurt Silsby <kdsilsby@gmail.com>
2024-08-21 16:35:56 -0400

    Update text fields to be more accurate. Also added general notes on units to be used for now and future changes wanted.

--- Density Altitude Calculator/ContentView.swift
+++ Density Altitude Calculator/ContentView.swift
@@ -5,6 +5,8 @@
 //  Created by Kurt Silsby on 8/21/24.
 //
 
+//Default US aviation units for weather are celsius (C), inches of mercury (inHg) and feet (ft). Rest of the world commonly uses celsius, hecto pascals (hPa) and meters (m). Common unit of temperature measurement in the US otherwise is fahrenheit (F) which can be an extra option since some old manuals have values in F instead of C for performance calculations. 
+
 import SwiftUI
 
 struct ContentView: View {
@@ -15,21 +17,24 @@ struct ContentView: View {
     var body: some View {
         VStack {
             HStack {
+                //Currently default altitude will be in feet. Add options later for meters
                 Text("Field Elevation: ")
-                TextField("Field Elevation", value: $elevation, format: .number)
+                TextField("Field Elevation in Feet", value: $elevation, format: .number)
             }
             HStack {
+                //Currently default temperature will be celsius. Will add option for fahrenheit.
                 Text("Temperature: ")
                 TextField("Temperature in C", value: $temperature, format: .number)
             }
             HStack {
+                //Current default altimeter units are inHg. Will add option for hPa
                 Text("Altimeter: ")
                 TextField("Altimeter in inHG", value: $altimeter, format: .number)
             }
             HStack {
-                Text("Dry Density Altitude Calculator")
+                Text("Dry Density Altitude: ")
             }
-            
+            .padding()
         }
         .padding()
     }
